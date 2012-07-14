864d202a9ef64ae684e94a7c3c6567a6b447d6c4
Jake Hickenlooper <jake@weboftomorrow.com>
2012-07-14 06:13:18 -0600

    vb 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import os
 
 name = "chill"
-version = "0.0.0"
+version = "0.1.0"
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
