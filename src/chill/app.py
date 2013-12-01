4dff16996778c3aa77fd85503f8c9a67cbc3a7a9
Jake Hickenlooper <jake@weboftomorrow.com>
2013-12-01 12:32:15 -0600

    Change to make_app

--- src/chill/app.py
+++ src/chill/app.py
@@ -20,13 +20,14 @@ from chill.resource import resource
 from chill.page import page
 from chill.tools import build_context_data
 
-def make_app(config, debug=False):
+def make_app(config, **kw):
     "factory to create the app"
 
     app = Flask(__name__)
 
     app.config.from_pyfile(config)
-    app.debug = debug
+    app.config.update(kw)
+    #app.debug = debug
 
     # register any blueprints here
     app.register_blueprint(resource)
